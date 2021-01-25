c9c14912eceac871a313636fb46b1973d3ee44fb
kerron <kgpsounds.com@gmail.com>
2021-01-25 18:59:16 -0400

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Twitter Linux
+
+[![Greenkeeper badge](https://badges.greenkeeper.io/kerrongordon/twitter-linux.svg)](https://greenkeeper.io/)
+[![Build/release](https://github.com/kerrongordon/twitter-linux/workflows/Build/release/badge.svg)](https://github.com/kerrongordon/twitter-linux/actions)
+
+Simple. Secure. Reliable messaging. With WhatsApp, you'll get fast, simple, secure messaging and calling for free\*, available on phones all over the world.
+
+## Download For Linux
+
+[Linux Packages](https://github.com/kerrongordon/twitter-linux/releases/latest)
+
+```bash
+sudo apt-get install build-essential
+sudo apt-get install --no-install-recommends -y rpm
+```
+
+## How To Use
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/)
+
+```bash
+
+# Clone this repository
+git clone https://github.com/kerrongordon/twitter-linux.git
+# Go into the repository
+cd twitter-linux
+# Install dependencies
+npm install
+# Run the app
+npm start
+# Build the app
+npm run build:linux
+```
---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { app, BrowserWindow, nativeImage, nativeTheme } from 'electron'
+import * as path from 'path'
+
+require('electron-context-menu')({
+  prepend: (params: Electron.ContextMenuParams) => [{
+    visible: params.mediaType === 'image',
+  }],
+})
+
+let mainWindow: Electron.BrowserWindow
+const appURL = 'https://twitter.com'
+const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36';
+const appName = 'WhatsApp'
+const bgColor = '#f2f2f2'
+
+const iconLink = nativeImage.createFromPath(path.join(__dirname, 'icon/1024x1024.png'))
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    backgroundColor: bgColor,
+    height: 700,
+    center: true,
+    icon: iconLink,
+    show: false,
+    width: 1200,
+  })
+
+  nativeTheme.themeSource = 'system'
+
+  mainWindow.loadURL(appURL, { userAgent })
+  mainWindow.setTitle(appName)
+  mainWindow.setAutoHideMenuBar(true)
+  mainWindow.setMenuBarVisibility(false)
+  mainWindow.on('ready-to-show', () => mainWindow.show())
+}
+
+app.on('ready', () => createWindow())
+app.on('window-all-closed', () => app.quit())
